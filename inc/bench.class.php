7e436cc2a78e99ddb6e32324e77656d757dad4a9
Arpad Borsos <arpad.borsos@googlemail.com>
2007-12-31 18:21:45 +0100

    update copyright header: new description, bump year and gpl3

--- inc/bench.class.php
+++ inc/bench.class.php
@@ -1,21 +1,20 @@
 <?php
 /*
- * dWing - a nextgen CMS built on PHP5, MySQLi and Ajax
- * Copyright (C) 2004-2007 Arpad Borsos <arpad.borsos@googlemail.com>
+ * dWing - a cms aimed to be as bleeding edge as possible
+ * Copyright (C) 2004-2008 Arpad Borsos <arpad.borsos@googlemail.com>
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
@@ -55,8 +54,7 @@ class bench
 	 **/
 	public function start()
 	{
-		$timetemp = explode(' ',microtime());
-		$this->start = $timetemp[0]+$timetemp[1];
+		$this->start = microtime(true);
 	}
 
 	/**
@@ -66,8 +64,7 @@ class bench
 	 **/
 	public function end()
 	{
-		$timetemp = explode(' ',microtime());
-		$this->time = $timetemp[0]+$timetemp[1]-$this->start;
+		$this->time = microtime(true)-$this->start;
 	}
 
 	/**
