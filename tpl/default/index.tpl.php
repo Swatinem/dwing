c4335eadb7cab9269bb7dce541320903513c2d3d
Arpad Borsos <arpad.borsos@googlemail.com>
2008-12-24 10:40:07 +0100

    on the way to a unified post template

--- tpl/default/index.tpl.php
+++ tpl/default/index.tpl.php
@@ -5,51 +5,12 @@ include($this->template('header.tpl.php'));
 // TODO: better tag cloud?
 // TODO: resurrect posting
 // TODO: implement voting as first RESTful service
-// TODO: ressurect comment posting
+// TODO: resurrect comment posting
 
 $iter = new NewsRange();
 
-foreach($iter as $news):
-?>
-<div class="post">
-	<div class="postheader">
-		<h1><a href="news/<?php echo $news->fancyurl; ?>"><?php echo htmlspecialchars($news->title); ?></a></h1>
-		<div class="postinfo">
-			<span class="userinfo"><a href="user/<?php echo $news->user->user_id; ?>">
-				<?php echo htmlspecialchars($news->user->nick); ?>
-			</a></span>
-			<span class="dateinfo"><?php echo Utils::relativeTime($news->time); ?></span>
-			<?php
-			// TODO: $news->tags, etc. accessors
-			$commentNum = Comments::getCommentNum($news->id, News::ContentType);
-
-			$thisRating = Ratings::getRating($news->id, News::ContentType);
-			$idStr = 'rating-'.$news->id.'-'.News::ContentType;
-			$jsParams = '\''.$idStr.'\', '.$news->id.', '.News::ContentType;
-			?>
-			<span class="tagsinfo">
-				<?php foreach($news->tags as $tag): ?>
-				<a href="news/tags/<?php echo $tag; ?>"><?php echo $tag; ?></a>
-				<?php endforeach; ?>
-			</span>
-			<?php if($commentNum > 0): ?>
-			<span class="commentinfo"><?php printf(l10n::_('%d comments'), $commentNum); ?></span>
-			<?php endif; ?>
-			<span class="rating score<?php echo round($thisRating['average']); ?>" id="<?php echo $idStr; ?>">
-				<a href="javascript:doRating(<?php echo $jsParams; ?>, 1);">1</a>
-				<a href="javascript:doRating(<?php echo $jsParams; ?>, 2);">2</a>
-				<a href="javascript:doRating(<?php echo $jsParams; ?>, 3);">3</a>
-				<a href="javascript:doRating(<?php echo $jsParams; ?>, 4);">4</a>
-				<a href="javascript:doRating(<?php echo $jsParams; ?>, 5);">5</a>
-				<span class="ratingcaption"><?php printf(l10n::_('%s ratings / %s average'), $thisRating['ratings'], round($thisRating['average'], 1)); ?></span>
-			</span>
-		</div>
-	</div>
-	<div class="postbody">
-<?php echo Smilies::replace($news->text); ?>
-	</div>
-</div>
-<?php
+foreach($iter as $post):
+	include($this->template('post.tpl.php'));
 endforeach;
 include($this->template('footer.tpl.php'));
 ?>
---
+++ tpl/default/post.tpl.php
@@ -0,0 +1,51 @@
+<?php
+if(empty($post))
+	throw new Exception('This template needs the $post variable.');
+?>
+<div class="post">
+	<div class="postheader">
+		<?php if(!empty($post->fancyurl) && !empty($post->title)): ?>
+		<h1><a href="news/<?php echo $post->fancyurl; ?>"><?php echo htmlspecialchars($post->title); ?></a></h1>
+		<?php endif; ?>
+		<div class="postinfo">
+			<?php if(($user = $post->user) != null): ?>
+			<span class="userinfo"><a href="user/<?php echo $user->user_id; ?>">
+				<?php echo htmlspecialchars($user->nick); ?>
+			</a></span>
+			<?php endif; ?>
+			<?php if(!empty($post->time)): ?>
+			<span class="dateinfo"><?php echo Utils::relativeTime($post->time); ?></span>
+			<?php endif; ?>
+			<?php
+			// TODO: $news->tags, etc. accessors
+			$commentNum = Comments::getCommentNum($post->id, News::ContentType);
+
+			$thisRating = Ratings::getRating($post->id, News::ContentType);
+			$idStr = 'rating-'.$post->id.'-'.News::ContentType;
+			$jsParams = '\''.$idStr.'\', '.$post->id.', '.News::ContentType;
+			$tags = $post->tags;
+			if(!empty($tags)):
+			?>
+			<span class="tagsinfo">
+				<?php foreach($tags as $tag): ?>
+				<a href="news/tags/<?php echo $tag; ?>"><?php echo $tag; ?></a>
+				<?php endforeach; ?>
+			</span>
+			<?php endif; ?>
+			<?php if($commentNum > 0): ?>
+			<span class="commentinfo"><?php printf(l10n::_('%d comments'), $commentNum); ?></span>
+			<?php endif; ?>
+			<span class="rating score<?php echo round($thisRating['average']); ?>" id="<?php echo $idStr; ?>">
+				<a href="javascript:doRating(<?php echo $jsParams; ?>, 1);">1</a>
+				<a href="javascript:doRating(<?php echo $jsParams; ?>, 2);">2</a>
+				<a href="javascript:doRating(<?php echo $jsParams; ?>, 3);">3</a>
+				<a href="javascript:doRating(<?php echo $jsParams; ?>, 4);">4</a>
+				<a href="javascript:doRating(<?php echo $jsParams; ?>, 5);">5</a>
+				<span class="ratingcaption"><?php printf(l10n::_('%s ratings / %s average'), $thisRating['ratings'], round($thisRating['average'], 1)); ?></span>
+			</span>
+		</div>
+	</div>
+	<div class="postbody">
+<?php echo Smilies::replace($post->text); ?>
+	</div>
+</div>
