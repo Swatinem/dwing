b003ac8ef3379a8ced4e70036c1d4b5ce1e889e6
Arpad Borsos <arpad.borsos@googlemail.com>
2008-12-24 14:22:47 +0100

    Add news template

---
+++ tpl/default/news.tpl.php
@@ -0,0 +1,21 @@
+<?php
+$title = $object->title;
+include($this->template('header.tpl.php'));
+
+$post = $object;
+	include($this->template('post.tpl.php'));
+?>
+<hr />
+<div class="area">
+	<h1><?php echo l10n::_('Comments'); ?></h1>
+</div>
+<hr />
+<?php
+foreach($object->comments as $post):
+	include($this->template('post.tpl.php'));
+endforeach;
+
+// TODO: resurrect comment posting
+
+include($this->template('footer.tpl.php'));
+?>
